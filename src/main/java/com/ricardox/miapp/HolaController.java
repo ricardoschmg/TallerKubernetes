25a4229690be9c2b05374ce34825bd26c489e079
ricardoschmg <ricardoschmg@gmail.com>
2025-04-12 23:00:35 -0500

    ajuste para  construccion de imagen  de docker

--- src/main/java/com/ricardox/miapp/HolaController.java
+++ src/main/java/com/ricardox/miapp/HolaController.java
@@ -8,7 +8,7 @@ public class HolaController {
 
     @GetMapping("/saludo")
     public String saludar() {
-        return "¡Hola, el microservicio está activo!";
+        return "¡Hola, el microservicio está activo RS!";
     }
 
 }
