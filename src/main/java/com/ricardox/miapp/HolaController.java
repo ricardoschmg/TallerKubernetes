5ccebf640dcc47f0323d492dbf835b406b23c544
ricardoschmg <ricardoschmg@gmail.com>
2025-04-13 23:07:28 -0500

    ajustes

--- src/main/java/com/ricardox/miapp/HolaController.java
+++ src/main/java/com/ricardox/miapp/HolaController.java
@@ -8,7 +8,7 @@ public class HolaController {
 
     @GetMapping("/saludo")
     public String saludar() {
-        return "¡Hola, el microservicio está activo RS!";
+        return "¡Hola, el microservicio está activo RS**";
     }
 
 }
